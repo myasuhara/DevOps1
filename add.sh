143c2b9e075df33a118b7f93a5c741837bf7f277
mitsuyo yasuhara <myasuhara@gmail.com>
2018-05-11 10:14:02 -0700

    commit

--- add.sh
+++ add.sh
@@ -3,3 +3,5 @@ echo "***********************************************************" >>$HOME/git-r
 echo `date` >> $HOME/git-repo/logs
 git add . 
 git commit -m "commit" >> $HOME/git-repo/logs  2>&1
+echo "***********************************************************" >>$HOME/git-repo/logs
+
