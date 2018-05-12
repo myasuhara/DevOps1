069f9d2ed6f09e8ea1108b6b745694fc091e0dd1
mitsuyo yasuhara <myasuhara@gmail.com>
2018-05-12 07:48:35 -0700

    del

--- add-devops1a.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git add $HOME/git-repo/devops1a.sh  
-git commit -m "commit" >> $HOME/git-repo/logs  2>&1
--- add-devops1b.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git add $HOME/git-repo/devops1b.sh  
-git commit -m "commit" >> $HOME/git-repo/logs  2>&1
